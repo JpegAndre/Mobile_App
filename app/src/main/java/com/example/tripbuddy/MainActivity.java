d21871b5bd2c54eba9e495e78c6d0b4ff91899f2
Andre J <andre.joubert99@gmail.com>
2025-08-15 10:41:19 +0200

    Got the home page with the nav bar to work

--- app/src/main/java/com/example/tripbuddy/MainActivity.java
+++ app/src/main/java/com/example/tripbuddy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tripbuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                handleLogin();
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                //handleLogin();
             }
         });
     }
