f1a5daa64403fbf3ca7286fc3cbb34a6f3841d7b
Andre J <andre.joubert99@gmail.com>
2025-08-21 14:44:06 +0200

    Further refinements to the splash screen and added a fade in

--- app/src/main/java/com/example/tripbuddy/Splash.java
+++ app/src/main/java/com/example/tripbuddy/Splash.java
@@ -3,6 +3,9 @@ package com.example.tripbuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,17 +16,27 @@ import androidx.core.splashscreen.SplashScreen;
 
 public class Splash extends AppCompatActivity {
 
+    TextView txtView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+        txtView = findViewById(R.id.textWelcome);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        new Handler().postDelayed(() -> {
+            Animation fadeIn = AnimationUtils.loadAnimation(Splash.this, R.anim.fade_in);
+            txtView.startAnimation(fadeIn);
+            txtView.setVisibility(TextView.VISIBLE);  // make sure it’s visible
+        }, 500);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -32,6 +45,6 @@ public class Splash extends AppCompatActivity {
                 startActivity(new Intent(Splash.this, MainActivity.class));
                 finish(); // Optional: closes the splash screen so user can't return to it
             }
-        }, 2000);
+        }, 3000);
     }
 }
